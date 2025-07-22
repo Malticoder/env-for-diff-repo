191bd0bc397632e02380ee4d10a395e2b0cad329
maltidemo <150908958+Malticoder@users.noreply.github.com>
2025-07-22 20:59:57 +0530

    Update backend.tf

--- terraform/modules/backend.tf
+++ terraform/modules/backend.tf
@@ -1,7 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "krishna-bucket-for-backend"
-    key            = "${var.environment}/terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
+
