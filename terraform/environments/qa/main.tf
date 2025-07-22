c3080f14f5e23bbe66d446e12f33ad4b0bad52ad
maltidemo <150908958+Malticoder@users.noreply.github.com>
2025-07-22 18:53:13 +0530

    Update main.tf

--- terraform/environments/qa/main.tf
+++ terraform/environments/qa/main.tf
@@ -1,31 +1,15 @@
-
-variable "vpc_id" {
-    type = string
+locals {
+  environment = "dev"
+  ami_id = "ami-0150ccaf51ab55a51"
+  instance_type = "t2.micro"
+  vpc_id = "vpc-0affcc89643f1e634"
+  subnet_id = "subnet-098e8c22744310155"
 }
-
-variable "environment" {
-    type = string
-}
-
-variable "ami_id" {
-    type = string
-}
-
-variable "instance_type" {
-    type = string
-}
-
-
-
-variable "subnet_id" {
-    type = string
-}
-
 module "web_sg" {
   source = "../../modules/security_group"
 
-  vpc_id = var.vpc_id
-  environment = var.environment
+  vpc_id = local.vpc_id
+  environment = local.environment
   ingress_rules = [
     {
       from_port   = 80
@@ -45,17 +29,9 @@ module "web_sg" {
 module "web_ec2" {
   source = "../../modules/ec2"
 
-  ami_id           = var.ami_id
-  instance_type    = var.instance_type
+  ami_id           = local.ami_id
+  instance_type    = local.instance_type
   security_group_ids = [module.web_sg.security_group_id]
-  subnet_id        = var.subnet_id
-  environment      = var.environment
-}
-
-locals {
-  environment = "dev"
-  ami_id = "ami-0150ccaf51ab55a51"
-  instance_type = "t2.micro"
-  vpc_id = "vpc-0affcc89643f1e634"
-  subnet_id = "subnet-098e8c22744310155"
+  subnet_id        = local.subnet_id
+  environment      = local.environment
 }
